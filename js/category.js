3d13ce8ef71340a615d6d83585cdecddb4229435
zhuhuiwen <zhuhuiwen@ifunq.com>
2016-11-19 23:17:33 +0800

    no message

--- js/category.js
+++ js/category.js
@@ -2,7 +2,7 @@
 * @Author: zhuhw
 * @Date:   2016-11-18 22:46:15
 * @Last Modified by:   Administrator
-* @Last Modified time: 2016-11-19 23:15:32
+* @Last Modified time: 2016-11-19 23:17:19
 */
 /*侧边栏可以拖动的效果
 1. touch事件
@@ -43,11 +43,11 @@ touchEvent(productWrap,productWrapHeight,cateRightHeight);
 touchEvent(lisWrap,lisWrapHeight,contentBoxHeight);
 
 
-document.body.ontouchmove=function(e){
-	alert(1);
+/*document.body.ontouchmove=function(e){
+	// alert(1);
     e.preventDefault();
 }
-
+*/
 /**
  * [touchEvent description]
  * @param  {[type]} lisWrap          [ul  lisWrap 移动的元素]
@@ -100,8 +100,8 @@ function touchEvent(lisWrap,lisWrapHeight,contentBoxHeight){
 
 		if(dY+currentY<maxV&&(dY+currentY)>-(lisWrapHeight-contentBoxHeight+maxV)){
 			//阻止滚屏
-			 // e.preventDefault();  
-			    e.stopPropagation();
+			e.preventDefault();  
+			   // e.stopPropagation();
     //停止冒泡
 			//跟着进行移动
 			this.style.webkitTransform = 'translateY('+(dY+currentY)+'px)';
